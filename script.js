3ad5439adfc03b8c4cf3c926f0ad928c91232a08
Randall LaPoint <rplapointjr@gmail.com>
2024-06-26 12:45:05 -0500

    Changed display messages to utilize template literals

--- script.js
+++ script.js
@@ -2,6 +2,7 @@
 let playerScore = 0;
 let computerScore = 0;
 
+
 // Returns one of the following choices at random
 const getComputerChoice = () => {
     const randomNumber = Math.floor(Math.random() * 3) + 1;
@@ -19,8 +20,8 @@ const getComputerChoice = () => {
 
 // Returns one of the valid choices based on user input
 const getPlayerChoice = () => {
-      let playerChoice = window.prompt("Which will you choose: rock, paper, or scissors?").toLowerCase();
-      return playerChoice;
+    let playerChoice = window.prompt("Which will you choose: rock, paper, or scissors?").toLowerCase();
+    return playerChoice;
 };
 
 // Determines the round winner, increments score, and displays the winning message
@@ -29,25 +30,25 @@ const playRound = (playerChoice, computerChoice) => {
     const playerWin = (playerChoice === "rock" && computerChoice === "scissors") ||
                       (playerChoice === "paper" && computerChoice === "rock") ||
                       (playerChoice === "scissors" && computerChoice === "paper");
-    
+  
     if (roundTie) {
-      console.log("It's a tie. Both of you chose " + computerChoice.toUpperCase() + "." );
+        console.log(`It's a tie. You both chose: ${computerChoice.toUpperCase()}` );
     } else if (playerWin) {
-      playerScore += 1;
-      console.log("You won! " + playerChoice.toUpperCase() + " beats " + computerChoice.toUpperCase() + "!");
+        playerScore += 1;
+        console.log(`You won! ${playerChoice.toUpperCase()} beats ${computerChoice.toUpperCase()}.`);
     } else {
-      computerScore += 1;
-      console.log("You lose! " + computerChoice.toUpperCase() + " beats " + playerChoice.toUpperCase() + "!");
-    }
-    displayScore(playerScore, computerScore);
+        computerScore += 1;
+        console.log("You lose! " + computerChoice.toUpperCase() + " beats " + playerChoice.toUpperCase() + "!");
+  }
+  
 }
 
 // Displays the score at the end of each round
 const displayScore = (playerScore, computerScore) => {
-  console.log("The current score is Player: " + playerScore + " and Computer: " + computerScore + ".");
+    console.log("The current score is Player: " + playerScore + " and Computer: " + computerScore + ".");
 }
 
-// 
+// Plays 5 rounds of the game, keeps track of the scores, and declares a winner
 
 const playerSelection = getPlayerChoice();
 const computerSelection = getComputerChoice();

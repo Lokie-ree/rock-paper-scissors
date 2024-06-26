f46ea1a5c3fca32e609391e0394adbc6c5556b4c
Randall LaPoint <rplapointjr@gmail.com>
2024-06-26 11:08:47 -0500

    playRound function logic added to script.js

--- script.js
+++ script.js
@@ -1,29 +1,48 @@
 // Step 1: Create variables to keep track of Human/Computer scores
-let humanScore = 0;
+let playerScore = 0;
 let computerScore = 0;
-
-
-// Step 2: Write the logic to get the computer's choice
 
 // Function that returns one of the following choices: rock, paper, scissors 
 const getComputerChoice = () => {
     const randomNumber = Math.floor(Math.random() * 3) + 1;
-        let computerChoice = "";
+        let computerChoice;
 
         if (randomNumber === 1) {
-            computerChoice += "Rock";
+            computerChoice = "rock";
         } else if (randomNumber === 2) {
-            computerChoice += "Paper";
+            computerChoice = "paper";
         } else {
-            computerChoice += "Scissors";
+            computerChoice = "scissors";
         }
     return computerChoice;
 };
 
-// Step 3: Write the logic to get the human's choice
-
 // Function that returns one of the valid choices based on user input
-const getHumanChoice = () => {
-      let humanDecision = window.prompt("Which will you choose: rock, paper, or scissors?");
-      return humanDecision.toLowerCase();
+const getPlayerChoice = () => {
+      let playerChoice = window.prompt("Which will you choose: rock, paper, or scissors?").toLowerCase();
+      return playerChoice;
 };
+
+// Function that determines the round winner, increments score, and displays the winning message
+const playRound = (playerChoice, computerChoice) => {
+    const roundTie = playerChoice === computerChoice;
+    const playerWin = (playerChoice === "rock" && computerChoice === "scissors") ||
+                      (playerChoice === "paper" && computerChoice === "rock") ||
+                      (playerChoice === "scissors" && computerChoice === "paper");
+    
+    if (roundTie) {
+      console.log("It's a tie. Both of you chose " + computerChoice + "." );
+    } else if (playerWin) {
+      playerScore += 1;
+      console.log("You won! " + playerChoice + " beats " + computerChoice + "!");
+    } else {
+      computerScore += 1;
+      console.log("You lose! " + computerChoice + " beats " + playerChoice + "!");
+    }
+}
+
+
+const playerSelection = getPlayerChoice();
+const computerSelection = getComputerChoice();
+
+playRound(playerSelection, computerSelection);
